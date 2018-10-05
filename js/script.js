fb518f2bc4bb40049659a5e234ed1742932baacd
admin <admin@iMac3.local>
2018-10-05 15:23:21 +0200

    add menu

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+  
+});
+// menu 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
